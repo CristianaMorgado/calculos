81535dfffe8cd21f55fb60b4561d8e879da15952
Cristiana Silva <146031663+CristianaMorgado@users.noreply.github.com>
2023-10-11 10:23:13 +0100

    Tornar a Classe Aritmética numa classe estática

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -9,7 +9,7 @@ namespace Calculos
     /// <summary>
     /// Implementa operações aritméticas.
     /// </summary>
-    internal class Aritmetica
+    internal static class Aritmetica
     {
 
         /// <summary>
@@ -26,7 +26,7 @@ namespace Calculos
         /// Operação Soma.
         /// </summary>
         /// <returns>Devolve a soma de dois números.</returns>
-        public int Somar(int x, int y)
+        public static int Somar(int x, int y)
         {
             return x + y;
         }
@@ -35,12 +35,12 @@ namespace Calculos
         /// Operação subtração.
         /// </summary>
         /// <returns>Devolve o resultado da subtração de dois números.</returns>
-        public int Subtrair(int x, int y)
+        public static int Subtrair(int x, int y)
         {
             return x - y;
         }
 
-        public double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+        public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
         {
             if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
             {
